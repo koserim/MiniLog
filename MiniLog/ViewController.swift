847629758895808be0db4a867f27b887f7c0d233
koserim <btobang1@naver.com>
2020-07-26 13:17:50 +0900

    Initial Commit

---
+++ MiniLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniLog
+//
+//  Created by 바보세림이 on 2020/07/26.
+//  Copyright © 2020 serim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
